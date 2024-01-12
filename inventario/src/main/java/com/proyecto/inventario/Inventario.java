59ca9cc72f5440c8c57063acddf6d620541ec74e
cronceroicloud <croncero@icloud.com>
2024-01-12 11:10:24 +0100

    Commit 1: Práctica realizada

---
+++ inventario/src/main/java/com/proyecto/inventario/Inventario.java
@@ -0,0 +1,17 @@
+/*
+ * Práctica de inventario
+ */
+
+package com.proyecto.inventario;
+
+/**
+ *
+ * @author daw1_
+ */
+public class Inventario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("SOLUCIÓN");
+    }
+}
