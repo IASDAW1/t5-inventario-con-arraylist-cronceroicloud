c6de37b1d08c055f32323cb57bdf655bac14fc6d
cronceroicloud <croncero@icloud.com>
2024-01-19 11:47:32 +0100

    Commit 1: Práctica realizada

---
+++ inventario2/src/main/java/com/proyecto/inventario2/Inventario2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.proyecto.inventario2;
+
+/**
+ *
+ * @author daw1_
+ */
+public class Inventario2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //ddfsfad
+    }
+}
